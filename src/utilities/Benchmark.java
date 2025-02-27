2e31c462153e23bef2b2764c4f883e9a335f4c30
BryanTeruel <164897126+BryanTeruel@users.noreply.github.com>
2025-02-27 01:46:05 -0700

    benchmark message

--- src/utilities/Benchmark.java
+++ src/utilities/Benchmark.java
@@ -1,14 +1,55 @@
 package utilities;
 
+/**
+ * Created on February 26, 2025
+ * @author bbteruel
+ * @version 1.0
+ */
+
 public class Benchmark {
+	String name;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	long start, stop, duration;
 
 	/**
+	 * @param name
 	 * @param start
 	 * @param stop
 	 * @param duration
 	 */
-	public Benchmark() {
+	
+	public Benchmark(String sortName) {
+		switch (sortName.toLowerCase()) {
+		case "b":
+			this.name = "Bubble Sort";
+			break;
+		case "i":
+			this.name = "Insertion Sort";
+			break;
+		case "s":
+			this.name = "Selection Sort";
+			break;
+		case "m":
+			this.name = "Merge Sort";
+			break;
+		case "q":
+			this.name = "Quick Sort";
+			break;
+		case "z":
+			this.name = "Custom Sort";
+			break;
+
+		default:
+			break;
+		}
 		this.start = 0;
 		this.stop = 0;
 		this.duration = 0;
@@ -24,5 +65,11 @@ public class Benchmark {
 	
 	public long result() {
 		return stop-start;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return name + " runtime was: " + result() + " milliseconds";
 	}
 }
